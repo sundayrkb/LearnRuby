279a54a41d017aaa1659251e23c8e8164cfeaa84
sodel <sodel@sodel-GA-78LMT-S2.(none)>
2016-01-14 12:20:12 +0530

    one program

---
+++ one.rb
@@ -0,0 +1,15 @@
+x=1
+if x > 2
+   puts "x is greater than 2"
+elsif x <= 2 and x!=0
+   puts "x is 1"
+else
+   puts "I can't guess the number"
+end
+$i = 0
+$num = 5
+
+while $i < $num  do
+   puts("Inside the loop i = #$i" )
+   $i +=1
+end
